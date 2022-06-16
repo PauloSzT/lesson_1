9989c1c8d7370fa816c256c2877c563874e07645
PauloSzT <pauloszt@gmail.com>
2022-06-15 23:00:48 -0500

    Rocket Mission

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,44 +1,49 @@
-fun main(args: Array<String>) {
-    execute(carTwo)
+import rocketmission.assets.Rocket
+import rocketmission.assets.Supplies
+import rocketmission.simulator.SimulatorImplementation
+lateinit var simulator: SimulatorImplementation
+fun main() {
+    simulator = SimulatorImplementation(rocketList = rocketList1, suppliesList = supplyList)
+    simulator.executeSimulation()
+
 }
 
-fun execute(car: MyFirstCarClass) {
-    println(car.year)
-    println(car.brand)
-    println(car.color)
-    println(car.weight)
-    println("additionalFeatures:")
-    for (elm in car.additionalFeatures) {
-        println("Feature: ${elm.name}, ${if(elm.isNew)"Is Brand New" else "Old Feature"}, Year: ${elm.featureYear}")
-    }
-}
-
-data class MyFirstCarClass(
-    val year: Int,
-    val brand: String,
-    val color: String,
-    val weight: Double,
-    val additionalFeatures: List<Feature>,
+val rocketList1: List<Rocket> = listOf(
+    Rocket(name = "Coete 1", maxLoad = 34.0, crashChance = 0.70),
+    Rocket(name = "Coete 2", maxLoad = 50.0, crashChance = 0.15),
+    Rocket(name = "Coete 3", maxLoad = 43.0, crashChance = 0.31)
 )
 
-data class Feature(
-    val isNew: Boolean,
-    val name: String,
-    val featureYear: Int,
+val supplyList: List<Supplies> = listOf(
+    Supplies(name = "Water", weight = 2.0, quantity = 2),
+    Supplies(name = "Food", weight = 3.0, quantity = 3),
+    Supplies(name = "Oxygen", weight = 1.3, quantity = 2),
+    Supplies(name = "Seeds", weight = 0.73, quantity = 10),
 )
 
-val carOne: MyFirstCarClass = MyFirstCarClass(
-    year = 2018,
-    brand = "Porsche",
-    color = "Blue",
-    weight = 3.07,
-    additionalFeatures = listOf(Feature(isNew = true, name = "New Rings", featureYear = 2018),Feature(isNew = false, name = "Electric Windows", featureYear = 2014))
-)
 
-val carTwo: MyFirstCarClass = MyFirstCarClass(
-    year = 2021,
-    brand = "Ferrari",
-    color = "Red",
-    weight = 2.9,
-    additionalFeatures = listOf(Feature(isNew = true, name = "New Rings", featureYear = 2021),Feature(isNew = false, name = "Electric Windows", featureYear = 2013))
-)
+
+
+
+
+
+
+
+
+
+
+//fun execute(car: MyFirstCarClass) {
+//    println(car.year)
+//    println(car.brand)
+//    println(car.color)
+//    println(car.weight)
+//    println("additionalFeatures:")
+//
+//    car.additionalFeatures.forEachIndexed { index, feat ->
+//        println("Lap: ${index + 1}")
+//        feat.forEachIndexed { subIndex, subFeat ->
+//            println( "SubLap: ${subIndex + 1}")
+//            println("Feature: ${subFeat.name}, ${if (subFeat.isNew) "Is Brand New" else "Old Feature"}, Year: ${subFeat.featureYear}")
+//        }
+//    }
+//}
---
+++ src/main/kotlin/rocketmission/simulator/SimulatorImplementation.kt
@@ -0,0 +1,47 @@
+package rocketmission.simulator
+
+import rocketmission.assets.Rocket
+import rocketmission.assets.Supplies
+
+class SimulatorImplementation(
+    val suppliesList: List<Supplies>,
+    val rocketList: List<Rocket>,
+): Simulator{
+    var supplies = suppliesList.toMutableList()
+    var rockets =rocketList.toMutableList()
+    override fun executeSimulation() {
+        loadRockets()
+    }
+    private fun loadRockets(){
+        rockets.forEach { rocket ->
+            val load = rocket.maxLoad
+            var sum = 0.0
+            supplies.forEach { supply ->
+                if (sum < load){
+                    rocket.container.add(supply)
+                    sum += (supply.weight * supply.quantity)
+                    supplies.remove(supply)
+                }
+            }
+        }
+        launch()
+    }
+    private fun launch(){
+        rockets.forEach { rocket ->
+            val random = (1..100).random()
+            val probability = rocket.crashChance.times(100)
+            if (random <= probability){
+                println ("${rocket.name}Rocket: Crash On TakeOff")
+            }else {
+                println ("${rocket.name}Rocket: Launch On TakeOff")
+                val randomLand = (1..100).random()
+                if (randomLand <= probability){
+                    println("${rocket.name}Rocket: Crash On Land")
+                }
+                else{
+                    println("${rocket.name} Rocket Landed on Mars and delivered ${rocket.container.size} Items")
+                }
+            }
+        }
+    }
+}
